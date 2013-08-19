00f75bb87eda0129dc1e0b9ba9d57d4c57c627d7
Gerald Nilles <geraldnilles@gmail.com>
2013-08-18 21:52:06 -0500

    Add DIAL module place holders

---
+++ sender/dial_discovery.py
@@ -0,0 +1,11 @@
+#-------------------
+# DIAL Discovery
+#
+# This module will be used to discover Chromecast devices
+#-------------------
+
+# Returns a list of Chromecast Devices
+def devices():
+	return []
+
+
---
+++ sender/dial_rest.py
@@ -0,0 +1,10 @@
+#------------------------
+# DIAL REST
+#
+# This module is used to launch Chromecast Apps
+#--------------------------
+
+
+def launch_app_id(app_id):
+	return True
+
