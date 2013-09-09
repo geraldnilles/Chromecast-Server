92761defd635d7c5c1a592dd54afc6affca1b0b8
Gerald Nilles <geraldnilles@gmail.com>
2013-09-08 22:02:36 -0500

    Create FCGI App

---
+++ server_app/fcgi_app.py
@@ -0,0 +1,26 @@
+
+def app():
+	# Look at path and call appropriate function
+	pass
+
+# Creates a list of movies.
+
+def list_movies():
+	pass
+
+def load_video():
+	pass
+
+def play_pause():
+	pass
+
+def skip():
+	pass
+
+def status():
+	pass
+
+if __name__ == "__main__":
+	print "Serving FastCGI app over WebSocket"
+
+
